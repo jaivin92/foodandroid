a081f76583f98d2ee27a73e026a432ca8508982b
Wijju9 <77786480+Wijju9@users.noreply.github.com>
2026-05-20 18:30:54 +0530

    Declare Kotlin Compose plugin version in root plugins block

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,5 @@
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.5.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.21")
-    }
+plugins {
+    id("com.android.application") version "8.5.2" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
 }
